3c061c055335b6340c23520eb0821c80f5ef6798
dao quy tien <tiendao@asimgroup.vn>
2026-03-11 17:31:41 +0700

    Init

---
+++ src/main/java/com/ecommerce/project/service/ProductService.java
@@ -0,0 +1,21 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.payload.ProductResponse;
+
+import java.util.List;
+
+
+public interface ProductService {
+
+    ProductDTO addProduct(Long categoryId, Product product);
+
+    ProductResponse getAllProducts();
+
+    ProductDTO getProductById(Long productId);
+
+    ProductResponse searchByCategory(Long categoryId);
+
+    ProductResponse searchProductByKeyword(String keyword);
+}
---
+++ src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -0,0 +1,87 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.exceptions.ResourceNotFoundException;
+import com.ecommerce.project.model.Category;
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.CategoryResponse;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.payload.ProductResponse;
+import com.ecommerce.project.repositories.CategoryRepository;
+import com.ecommerce.project.repositories.ProductRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final CategoryRepository categoryRepository;
+    private final ProductRepository productRepository;
+    private final ModelMapper modelMapper;
+
+    public ProductServiceImpl(CategoryRepository categoryRepository, ProductRepository productRepository) {
+        this.categoryRepository = categoryRepository;
+        this.productRepository = productRepository;
+        this.modelMapper = new ModelMapper();
+    }
+
+
+    @Override
+    public ProductDTO addProduct(Long categoryId, Product product) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
+
+        product.setImage("default.png");
+        product.setCategory(category);
+        double specialPrice = product.getPrice() -  (product.getDiscount() * 0.01) * product.getPrice();
+        product.setSpecialPrice(specialPrice);
+
+        Product savedProduct = productRepository.save(product);
+        return modelMapper.map(savedProduct, ProductDTO.class);
+    }
+
+    @Override
+    public ProductResponse getAllProducts() {
+      List<Product> products =   productRepository.findAll();
+      List<ProductDTO> productDTOS = products.stream()
+              .map(product -> modelMapper.map(product , ProductDTO.class))
+              .toList();
+      ProductResponse productResponse = new ProductResponse();
+      productResponse.setContent(productDTOS);
+      return productResponse;
+    }
+
+    @Override
+    public ProductDTO getProductById(Long productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Product", "productId", productId));
+        return modelMapper.map(product, ProductDTO.class);
+    }
+
+    @Override
+    public ProductResponse searchByCategory(Long categoryId) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
+        List<Product> products =   productRepository.findByCategoryOrderByPriceAsc(category);
+        List<ProductDTO> productDTOS = products.stream()
+                .map(product -> modelMapper.map(product , ProductDTO.class))
+                .toList();
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.setContent(productDTOS);
+        return productResponse;
+    }
+
+    @Override
+    public ProductResponse searchProductByKeyword(String keyword) {
+
+        List<Product> products =   productRepository.findByProductNameLikeIgnoreCase( '%' +keyword+ '%');
+        List<ProductDTO> productDTOS = products.stream()
+                .map(product -> modelMapper.map(product , ProductDTO.class))
+                .toList();
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.setContent(productDTOS);
+        return productResponse;
+    }
+
+}
